8e8c634d04a996eaa9cb2d3950ad33eaa7845991
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 18:57:55 -0500

    Node.js Errors: Manejo de errores en Node.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const  errors = require("./src/errors")
+
+//try{
+ //   errors.standardErr.range();
+//}catch(err){
+  //  console.log("Ha ocurrido un error:", //err)
+//}
+const handle = require("./src/handling")
+handle.errorFirstCallbackWrong()
