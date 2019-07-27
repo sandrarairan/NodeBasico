daad88573c5c94d89b683c9fa9ee3a18a428dcf2
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 15:40:13 -0500

    Hacia el futuro: asincronismo con Async/Await

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call")
+
+//call.greet("Sandra Rairan")
+async function callWithPromise(){
+   const fullName = await call.withPromise("SANDRA","RAIRAN")
+   console.log(fullName)
+}
+callWithPromise()
