9b41575954860b259882c65a5792148f798fac57
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 17:55:13 -0500

    Node Child Process Ejecución subprocesos con Node

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process');
+
+function excmm(command){
+   cp.exec(command, (err, stdout, sterr) => {
+        if(err) throw err;
+        if(stdout) console.log(`Out: \n${stdout}`);
+        if(sterr) console.log(`sterr: \${sterr}`);
+    })
+}
+
+//excmm("ls");
+excmm("node ./src/script --base=7 --f=1e5");
