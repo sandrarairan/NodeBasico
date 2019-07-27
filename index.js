54fb9d61df65806e80b303832a68210715bd9df7
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 16:39:28 -0500

    Node FileSystem Lectura/Escritura archivos en Node

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs")
+const aps = require("./src/fileops")
+let incValue;
+fs.readFile("./resources/number.txt", "Utf8",(err,text)=>{
+    if(err) throw err;
+    const numbers = text.split("\n").map(n=>Number(n));
+    
+    incValue=aps.incrementValues(numbers)
+    fs.writeFile("./resources/numbernew.txt", incValue.join("\n"), (err,result)=>{
+        if(err) throw err;
+});
+})
