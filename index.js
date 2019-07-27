1b62ba8b2c113cb0389377ef1f3d7d46498adcfb
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 14:21:01 -0500

    Fundamentos de asincronismo con Node.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const add = require("./src/addition");
+const sin = require("./src/intensive")
+add.syncSum(1,2);
+add.asyncSum(2,4);
+
+sin.simulateAsync();
