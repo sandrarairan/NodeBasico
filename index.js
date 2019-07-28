023a580d49868ae9f2fdf98007a68b247b874869
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 20:17:16 -0500

    Creación de un servidor web básico con HTTP

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+const route = "./resources/my_page.html";
+
+
+const server = http.createServer((req, res ) => {
+
+	fs.readFile(route, (err, data ) => {
+		if(err) { console.error(err); return;}
+
+		res.end(data);
+	})
+})
+
+console.log(`Iniciando el servidor en el puerto ${port}`); 	
+server.listen(port)
