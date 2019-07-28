7dc5292ab1671c16aff03082e87aad002c705c10
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 19:17:55 -0500

    Node.js Events: Emisión y recepción de eventos en Node.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require("./src/greet")
+
+ greet.emit("clap") //Hacer llamado al evento sin argumento
+ greet.emit("greet", "CC") //Hacer llamado al evento, con un argumento
+
+ greet.emit("call", "sandra rairan" ,name=> { //Llamado a evento con función Callback
+     console.log(`Hi! ${name}`)
+ })
