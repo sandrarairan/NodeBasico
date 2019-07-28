f7ed091605c16cf1aea5b8d3a3a6cb1fc7a7b437
Sandra Rairan <sandrarairan@gmail.com>
2019-07-27 20:41:05 -0500

    Creación de un servidor web con Express

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express")
+const app = express();
+const fs =require("fs")
+ 
+app.get("/app",(req,res)=>{
+    res.end("Estoy en la ruta /app")
+  })
+
+app.get("/",(req,res)=>{
+    res.send("Estoy en Home")
+  })
+
+
+
+app.get("*",(req,res)=>{
+  res.send("No se donde estoy")
+})
+
+app.listen(3000,()=>{
+    console.log("El servidor express está escuchando en el puerto 3000")
+  })
